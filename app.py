b6c5bc5cf6a263cceb925d0732da36f74456aa4a
Masum Hasan <masumhasan92@gmail.com>
2023-04-25 11:51:05 +0600

    up

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ import pandas as pd
 import pickle
 st.title('UFlix Movie Recommender AI')
 
+mList = pickle.load(open('movies.pkl','rb'))
+mList = mList['title'].values
 
 option = st.selectbox(
     'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone'))
+    mList)
 
 st.write('You selected:', option)
+
+import streamlit as st
+
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
